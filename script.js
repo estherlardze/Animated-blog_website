d546da76c382f1351bf5852f8150ef76c33c447b
estherlardze <estherlardze8@gmail.com>
2023-06-25 23:17:24 +0000

    fixed minor issues

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    dots: true,
-    stagePadding: 0,
-    responsiveBaseElement: 'body',
-    responsive: {
-        0: {
-            items: 1,
-            nav: false
-        },
-        600: {
-            items: 2,
-            nav: false
-        },
-        1000: {
-            items: 3,
-            nav: false
-        },
-       
-    },
-});
-
 AOS.init();
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
@@ -33,3 +8,56 @@ toggleButton.addEventListener('click', ()=>{
     navLinks.classList.toggle('active')
     signLinks.classList.toggle('active')
 })
+
+// dummy data for recent post
+const latestPost = [
+     {
+        index:1,
+        image:"./images/post-3.jpg",
+        description: "Lorem ipsum dolor sit amet.",
+        category:"fashion",
+        time:"10 minutes ago"
+     },
+     {  index:2,
+        image:"./images/men.jpg",
+        description: "Lorem ipsum dolor sit amet.",
+        category:"fashion",
+        time:"2 hours ago"
+     },
+     {  index:3,
+        image:"./images/Ai.jpg",
+        description: "Lorem ipsum dolor sit amet.",
+        category:"fashion",
+        time:"2 minutes ago"
+     },
+     {  index:4,
+        image:"./images/ocean.jpg",
+        description: "Lorem ipsum dolor sit amet.",
+        category:"fashion",
+        time:"1 hour ago"
+     },
+     {  index:5,
+        image:"./images/program.jpg",
+        description: "Lorem ipsum dolor sit amet.",
+        category:"fashion",
+        time:"40 minutes ago"
+     }
+
+]
+ 
+const postCards = document.querySelector(".cards-section");
+const Posts = latestPost.map((item)=>{
+ return(
+    `<article key=${item.index}>
+      <img src=${item.image} alt="">
+      <h5 class="fw-bold">${item.description}</h5>
+      <button>${item.category}</button>
+      <p>${item.time}</p>
+   </article>`
+)}
+)
+ postCards.innerHTML =  Posts;
+ 
+
+ // dummy data for recent blog
+
